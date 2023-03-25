17a6dd4ba40ece14bd57d26d622366e0703a7675
thomasgravy <gravythomas@gmail.com>
2023-03-25 23:48:05 +0100

    init app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BowlingLanesModule} from './bowling-lanes/bowling-lanes.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'bowling-lanes', pathMatch: 'full' },
+  { path: 'bowling-lanes', loadChildren: () => import('./bowling-lanes/bowling-lanes.module').then(m => m.BowlingLanesModule) },
+  { path: '**', redirectTo: 'bowling-lanes' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BowlingLanesComponent } from './bowling-lanes/bowling-lanes.component';
+import { BowlingLanesRoutingModule } from './bowling-lanes/bowling-lanes-routing.module';
+import { BowlingLanesModule } from './bowling-lanes/bowling-lanes.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BowlingLanesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BowlingLanesRoutingModule,
+    BowlingLanesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
